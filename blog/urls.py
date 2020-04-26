4678b92bdbba55d81412265bbb0d35dc3f7adc59
mariusz <mnurski@gmail.com>
2020-04-26 15:18:10 +0200

    Django Blog gotowy działający

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('comments/', views.comment_list, name='comment_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/publish_draft_list/', views.post_publish_draft_list, name='post_publish_draft_list'),
+    #path('post/<pk>/delete/', views.post_delete, name='post_delete'),
+    #path('post/<pk>/delete_draft_list/', views.post_delete_draft_list, name='post_delete_draft_list'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve_redirect/<str:redirect_list>/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/unapprove_redirect/<str:redirect_list>/', views.comment_unapprove, name='comment_unapprove'),
+    path('post/<int:postpk>/comment/<int:pk>/delete_redirect/<str:redirect_list>/', views.comment_delete, name='comment_delete'),
+    path('post/<int:pk>/delete_redirect/<str:redirect_list>/', views.post_delete_redirect, name='post_delete_redirect'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,105 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from django.utils import timezone
-from .models import Post
+from django.contrib.auth.decorators import login_required
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+
 # Create your views here.
+def post_delete_redirect(request, pk, redirect_list):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect(redirect_list)
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts' : posts})
+
+def comment_list(request):
+    posts = Post.objects.order_by('-created_date')
+    return render(request, 'blog/comment_list.html', {'posts' : posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+#            post.publish_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form':form})
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+#            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_publish_draft_list(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_draft_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+def comment_approve(request, pk, redirect_list):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    if redirect_list == 'comment_list':
+        return redirect(redirect_list)
+    else:
+        return redirect(redirect_list, pk=comment.post.pk)
+
+def comment_unapprove(request, pk, redirect_list):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.unapprove()
+    if redirect_list == 'comment_list':
+        return redirect(redirect_list)
+    else:
+        return redirect(redirect_list, pk=comment.post.pk)
+
+def comment_delete(request, postpk, pk, redirect_list):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    if redirect_list == 'comment_list':
+        return redirect(redirect_list)
+    else:
+        return redirect(redirect_list, pk=comment.post.pk)
